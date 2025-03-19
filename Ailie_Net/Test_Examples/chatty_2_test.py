Futher development of example/test programs. Bug found and resolved in derivative of weight calculation.

Ailie_Net/Test_Examples/chatty_2_test.py
@@ -3,6 +3,8 @@
 import matplotlib.pyplot as plt
 # Importing external numpy maths library
 import numpy as np
+# importing the library to read JSON files
+import json
 
 # Importing included Ailey_Net package
 from Ailie_Net import *
@@ -12,33 +14,113 @@ from Ailie_Net import *
 epochs: The chosen number of training iterations to fit the Neural Net (Recommended: 100)
 learn_rate: The size of the step size within each training iteration (Typically 0.01 -> 0.001)
 """
-epochs = 100
+epochs = 400
 learn_rate = 0.01
 
 # Create list to track errors over time
 error_log = []
 
 # Known words the user might say
-vocab = ["hello", "what", "is", "your", "name", "hi", "who", "are", "you", "bye", "goodbye"]
-
+# vocab = ["hello", "what", "is", "your", "name", "hi", "who", "are", "you", "bye", "goodbye"]
+#
 # Available responses for the computer
-phrasebook = ["hello", "my name is Ailie", "hey", "i am Ailie", "bye", "see ya"]
+# phrasebook = ["hello", "my name is Ailie", "hey", "i am Ailie", "bye", "see ya"]
 
 # Training data of sample phrases for the computer to detect
-training_data = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],])
+# training_data = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#                           [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+#                           [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+#                           [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+#                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+#                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],])
 
 # Training targets of desired outputs or detectable phrases
-training_targets = np.array([[1, 0, 1, 0, 0, 0],
-                            [0, 1, 0, 1, 0, 0],
-                            [1, 0, 1, 0, 0, 0],
-                            [0, 1, 0, 1, 0, 0],
-                            [0, 0, 0, 0, 1, 1],
-                            [0, 0, 0, 0, 1, 1]])
+# training_targets = np.array([[1, 0, 1, 0, 0, 0],
+#                             [0, 1, 0, 1, 0, 0],
+#                             [1, 0, 1, 0, 0, 0],
+#                             [0, 1, 0, 1, 0, 0],
+#                             [0, 0, 0, 0, 1, 1],
+#                             [0, 0, 0, 0, 1, 1]])
+
+
+
+
+def load_examples_json(json_file: str):
+    # Opens the file containing the chat script
+    file = open(json_file)
+
+    # generate dictionary from json file
+    chat_script = json.load(file)
+
+    prompt_list = []
+    response_list = []
+
+    for example in chat_script["Chat_Scripts"]:
+        # Put stored phrases in respective lists
+        prompt_list.append(example["input"].lower())
+        response_list.append(example["response"])
+
+    file.close()
+
+    return prompt_list, response_list
+
+
+def gen_chat_training(prompt_list, response_list, vocabulary, phrasebook):
+    training_data = []
+    training_targets = []
+
+    for prompt, response in zip(prompt_list, response_list):
+        data_temp = [0 for i in range(len(vocabulary))]
+        targets_temp = [0 for i in range(len(phrasebook))]
+        # find which indexes the prompts words exist in vocabulary
+        for word in prompt.split():
+            vocab_index = vocabulary.index(word)
+            # one-hot encode detected vocab word
+            data_temp[vocab_index] = 1
+
+        # find which index the phrase exists in phrasebook
+        phrase_index = phrasebook.index(response)
+        # one-hot encode detected phrase
+        targets_temp[phrase_index] = 1
+
+        # add encodings to training lists
+        training_data.append(data_temp)
+        training_targets.append(targets_temp)
+
+    return training_data, training_targets
+
+
+def build_vocab(prompt_list: list) -> list:
+    vocab_builder = []
+    # collate prompt words into a list of unique words
+    for prompt in prompt_list:
+        for word in prompt.split():
+            if word not in vocab_builder:
+                vocab_builder.append(word)
+
+    return vocab_builder
+
+
+def build_phrasebook(response_list: list) -> list:
+    phrasebook_builder = []
+    # collate prompt words into a list of unique words
+    for phrase in response_list:
+        if phrase not in phrasebook_builder:
+            phrasebook_builder.append(phrase)
+
+    return phrasebook_builder
+
+chat_data_file = "chat_data.json"
+user_prompts, target_responses = load_examples_json(chat_data_file)
+print(f"\nuser prompts {user_prompts}")
+print(f"\ntarget responses {target_responses}")
+vocabulary = build_vocab(user_prompts)
+print(f"\nvocabulary {vocabulary}")
+phrasebook = build_phrasebook(target_responses)
+print(f"\nphrasebook {phrasebook}")
+training_data, training_targets = gen_chat_training(user_prompts, target_responses, vocabulary, phrasebook)
+print(f"\ntraining data {training_data}")
+print(f"\ntraining targets {training_targets}")
 
 def train_network(network: AilieNet, inputs, targets, error_log):
     for itt in range(0, epochs):
@@ -96,12 +178,16 @@ def output_decoder(phrasebook_encoding, phrasebook):
 
 
 # Creating the Neural Layers
-layer0 = Dense(8, 11)
-layer2 = Dense(6, 8)
+input_size = len(training_data[0])
+output_size = len(training_targets[0])
+layer0 = Dense(8, input_size)
+layer1 = Dense(8, 8)
+layer2 = Dense(output_size, 8)
 
 # Creating the Neural Network
 neuralNet = AilieNet()
 neuralNet.add(layer0)
+neuralNet.add(layer1)
 neuralNet.add(layer2)
 
 # Prompting the user to begin training
@@ -125,7 +211,7 @@ while chatting:
         chatting = False
 
     # Encoding the users message
-    vocab_encoding = input_encoder(input_phrase, vocab)
+    vocab_encoding = input_encoder(input_phrase.lower(), vocabulary)
     print(f"\nYou > {input_phrase}\nInput Encoding: {vocab_encoding}")
 
     # Generating the computers response

Ailie_Net/layer.py
@@ -28,8 +28,8 @@ class Dense:
 
     :param neurons: The quantity of neurons in this layer.
     :param input_size: The number of weighted connections to the previous layer.
-    :type neurons: int
-    :type input_size: int
+    :type neurons: int.
+    :type input_size: int.
 
     :var self.weight: 2D Matrix to store layer weight values.
     :var self.bias: 1D Matrix to store bias values.
@@ -41,6 +41,7 @@ class Dense:
     Create a layer with 8 neurons, each taking 11 internally weighted inputs.
 
     """
+
     def __init__(self, neurons: int, input_size: int):
         self.neurons = neurons
         self.input_size = input_size
@@ -49,6 +50,10 @@ class Dense:
         self.weighted_sum = np.zeros(neurons)
         self.inputs = np.zeros(input_size)
         self.outputs = np.zeros(neurons)
+        self.dcost_dweight = None
+        self.dcost_dlayer = None
+        self.dcost_dbias = None
+        self.dcost_dinput = None
 
     def size(self) -> None:
         """ Displays the current matrix sizes used by the layer """
@@ -101,13 +106,14 @@ class Dense:
 
         self.dcost_dlayer = layer_error
         for i in range(0, self.neurons):
-            self.dcost_dweight[i] = self.inputs * layer_error[i]
+            #self.dcost_dweight[i] = self.inputs * layer_error[i]
+            self.dcost_dweight[i] = np.dot(self.inputs, layer_error[i])
         # self.dcost_dweight = self.inputs.dot(layer_error)
         self.dcost_dbias = layer_error * 1
         # self.dcost_dinput = self.weight.T * layer_error
         self.dcost_dinput = self.weight.T.dot(layer_error)
         return self.dcost_dinput
 
-    def update(self, alpha):
+    def update(self, alpha: float) -> None:
         self.weight = self.weight - (alpha * self.dcost_dweight)
         self.bias = self.bias - (alpha * self.dcost_dbias)